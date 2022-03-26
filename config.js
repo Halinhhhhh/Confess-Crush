824685432e1609463959a83248feaa2488d03046
Halinhhhhh <102419332+Halinhhhhh@users.noreply.github.com>
2022-03-26 21:31:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Pettcuteeeee",
-    introTitle: 'Lê Tiến Quang',
+    titleWeb:  'Pettcuteeeee',
+    introTitle: 'Lê Tiến Quang'
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng anh đã yêu em ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    Đằng sau chữ yêu đây là thương`
+    btnIntro: '^^HiHi^^'
+    title: 'Phải chăng anh đã yêu em ngay từ cái nhìn đầu tiên 🥰'
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy '
+    btnYes: 'Thích lắm <33'
+    btnNo: 'Không nha :3'
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3'
+    btnReply: 'Gửi cho bạn <3'
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333'
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Còn chần chừ gìii mà khum inbox cho mình iiii.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Còn chần chừ gìii mà khum inbox cho mình iiii.'
+    btnAccept: 'Okiiiii lun <3'
     messLink: 'https://www.facebook.com/dhalinh230807'
